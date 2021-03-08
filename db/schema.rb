1250173d302abd44d3e8bcfe0a6afb1da13a75ed
Denis Naughton <mjolinor75@hotmail.com>
2021-03-07 22:09:12 -0500

    Migrated scaffold database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_232802) do
+ActiveRecord::Schema.define(version: 2021_03_08_030643) do
+
+  create_table "notebooks", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notebooks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_03_07_232802) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notebooks", "users"
 end
